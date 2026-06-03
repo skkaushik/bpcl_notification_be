b4ff892908313a2cd3b176b700302a2f62088dfd
sachin-zaggle <sachin.kaushik@zaggle.in>
2026-06-03 13:53:51 +0530

    code

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -2,17 +2,18 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // true for 465, false for other ports
+  port: 587,          // changed from 465
+  secure: false,      // false for 587 (STARTTLS)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  socketTimeout: 10000,
+  connectionTimeout: 30000,
+  socketTimeout: 30000,
   tls: {
     rejectUnauthorized: false
-  }
+  },
+  family: 4,          // force IPv4
 });
 
 module.exports = transporter;
