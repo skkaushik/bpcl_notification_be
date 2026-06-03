2bd944d43eee99f727bfb42d63e4ee9f5292f098
Damini Singh <daminisingh9761@gmail.com>
2026-06-03 13:09:48 +0530

    config change

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 module.exports = transporter;
